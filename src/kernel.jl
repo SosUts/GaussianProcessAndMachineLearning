4bd2a089015fa9d854363c6add6b7a5358f7fac1
SosUts <uts.199.ubosou@gmail.com>
2022-05-22 12:31:11 +0900

    Move posdef func to covariance.jl

--- src/kernel.jl
+++ src/kernel.jl
@@ -135,24 +135,3 @@ end
 function Base.:+(k1::SingleKernel, k2::ProductKernel)
     push!(k2.kernels, k1)
 end
-
-"""
-https://github.com/STOR-i/GaussianProcesses.jl/blob/9fe174a1753796fe7295b81d76536e5eb1c37f32/src/GP.jl#L91-L116
-"""
-function make_posdef!(m::AbstractMatrix, chol_factors::AbstractMatrix; nugget=1.0e-10)
-    n = size(m, 1)
-    size(m, 2) == n || throw(ArgumentError("Covariance matrix must be square"))
-    if nugget > 0
-        @inbounds for i in 1:n
-            m[i, i] += nugget
-        end
-    end
-    copyto!(chol_factors, m)
-    chol = cholesky!(Symmetric(chol_factors, :U))
-    m, chol
-end
-
-function make_posdef!(m::AbstractMatrix; nugget=1.0e-10)
-    chol_buffer = similar(m)
-    make_posdef!(m, chol_buffer; nugget=nugget)
-end
